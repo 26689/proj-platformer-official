9bf3d4b89d5db104440e1d92124732dc49f04151
26689 <tasso.mevissen@gmail.com>
2019-04-07 12:51:35 +0200

    commit 7-4-2019
    heb de verbetering van de camera movment af gemaakt en ben nu bezig met het karakter te laten draaien met de muis.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,17 +7,26 @@ public class CameraController : MonoBehaviour
 
     public Transform target;
     public Vector3 offset;
+    public bool useOffsetValues;
+    public float rotateSpeed;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = target.position - transform.position;
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get the x posistion of the mouse & rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
         transform.position = target.position - offset;
 
         transform.LookAt(target);
