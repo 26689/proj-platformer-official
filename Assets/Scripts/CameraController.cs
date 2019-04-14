54486c972161ce6f83726f95b67da4215094b44d
26689 <tasso.mevissen@gmail.com>
2019-04-14 13:15:54 +0200

    commit 14-4-2019
    kan nu camera draaien met de muis in het spel.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -27,7 +27,17 @@ public class CameraController : MonoBehaviour
         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
         target.Rotate(0, horizontal, 0);
 
-        transform.position = target.position - offset;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        target.Rotate(-vertical, 0, 0);
+
+        //move the camera based on current rotation of the target & the original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = target.eulerAngles.x;
+       
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
 
         transform.LookAt(target);
     }
